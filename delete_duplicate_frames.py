c272c3516faf4e2c1f14171680d727aa7e0ab0e8
Víctor Uceda <VictorUceda@users.noreply.github.com>
2022-05-10 18:18:49 +0200

    Update delete_duplicate_frames.py

--- delete_duplicate_frames.py
+++ delete_duplicate_frames.py
@@ -7,14 +7,14 @@ def is_similar(image1, image2):
 args = parser.parse_args()
 video_path = args.label_video_path
 
-out_video_root = video_path[:-3]+'_c.mp4'
+out_video_root = video_path[:-4]+'_c.mp4'
 
 print(video_path)
 cap = cv2.VideoCapture(video_path)
 fps = cap.get(cv2.CAP_PROP_FPS)
 size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-#videoWriter = cv2.VideoWriter(out_video_root, fourcc, fps, size)
+videoWriter = cv2.VideoWriter(out_video_root, fourcc, fps, size)
 i = 0
 last_img = None
 while (cap.isOpened()):
